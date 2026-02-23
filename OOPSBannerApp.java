98a6a23017da6bcab77376a0e11978c5b208a150
Thejas p <tp6959@srmist.edu.in>
2026-02-23 18:32:53 +0530

    UC4 compleated

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -2,15 +2,22 @@ public class OOPSBannerApp {
 
     public static void main(String[] args) {
 
-        // Line 1: Top caps of O, O, P and S
-        System.out.println(String.join(" ","   ****    ","   ****    ","**********  ","**********"));
-        System.out.println(String.join(" "," **     ** "," **     ** ","*********** ","**********"));
-        System.out.println(String.join(" ","**       **","**       **","**        **","**        "));
-        System.out.println(String.join(" ","**       **","**       **","**        **","**        "));
-        System.out.println(String.join(" ","**       **","**       **","**       ** ","**********"));
-        System.out.println(String.join(" ","**       **","**       **","**********  ","**********"));
-        System.out.println(String.join(" ","**       **","**       **","***         ","        **"));
-        System.out.println(String.join(" "," **     ** "," **     ** ","***         ","        **"));
-        System.out.println(String.join(" ","   ****    ","   ****    ","***         ","**********"));
+        // 1. Create a String array to store all lines of the banner
+        String[] bannerLines = {
+            String.join(" ","   ****    ","   ****    ","**********  ","**********"),
+            String.join(" "," **     ** "," **     ** ","*********** ","**********"),
+            String.join(" ","**       **","**       **","**        **","**        "),
+            String.join(" ","**       **","**       **","**        **","**        "),
+            String.join(" ","**       **","**       **","**       ** ","**********"),
+            String.join(" ","**       **","**       **","**********  ","**********"),
+            String.join(" ","**       **","**       **","***         ","        **"),
+            String.join(" "," **     ** "," **     ** ","***         ","        **"),
+            String.join(" ","   ****    ","   ****    ","***         ","**********"),
+        };
+
+        // 2. Use a for-each loop to print each element sequentially
+        for (String line : bannerLines) {
+            System.out.println(line);
+        }
     }
 }
