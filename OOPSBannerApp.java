d2e2442ed04fef5e94d01b2d3331b82b871fe3dd
Thejas p <tp6959@srmist.edu.in>
2026-02-24 08:57:58 +0530

    UC5 completed

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,7 +1,6 @@
 public public class OOPSBannerApp {
 
     public static void main(String[] args) {
-        // UC5: Combine array declaration and initialization with String.join()
         String[] bannerLines = new String[] {
         String.join("", "    *    " , "    *    " , " ***    "   , "    *** "),
         String.join("", "  *   *  " , "  *   *  " , " *     *  "   , "  **      "),
@@ -13,8 +12,6 @@ public public class OOPSBannerApp {
         String.join("", "  *   *  " , "  *   *  " , " *         "   , "     *   "),
         String.join("", "    *    " , "    *    " , " *         "   , "**     ")
         };
-
-        // Enhanced for-loop to iterate through the array and display the pattern
         for (String line : bannerLines) {
             System.out.println(line);
         }
