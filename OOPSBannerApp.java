288b891eee1a4e56a6a18375c753e1b4e32bc8d9
Thejas p <tp6959@srmist.edu.in>
2026-02-24 08:55:54 +0530

    UC5 completed

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,16 +1,24 @@
-public class OOPSBannerApp {
+public public class OOPSBannerApp {
 
     public static void main(String[] args) {
+        // UC5: Combine array declaration and initialization with String.join()
+        String[] bannerLines = new String[] {
+        String.join("", "    *    " , "    *    " , " ***    "   , "    *** "),
+        String.join("", "  *   *  " , "  *   *  " , " *     *  "   , "  **      "),
+        String.join("", " *     * " , " *     * " , " *      * "   , "**        "),
+        String.join("", " *     * " , " *     * " , " *     *  "   , " **       "),
+        String.join("", " *     * " , " *     * " , " ***    "   , "   *    "),
+        String.join("", " *     * " , " *     * " , " *         "   , "      *  "),
+        String.join("", " *     * " , " *     * " , " *         "   , "       * "),
+        String.join("", "  *   *  " , "  *   *  " , " *         "   , "     *   "),
+        String.join("", "    *    " , "    *    " , " *         "   , "**     ")
+        };
 
-        // Line 1: Top caps of O, O, P and S
-        System.out.println(String.join(" ","   ****    ","   ****    ","**********  ","**********"));
-        System.out.println(String.join(" "," **     ** "," **     ** ","*********** ","**********"));
-        System.out.println(String.join(" ","**       **","**       **","**        **","**        "));
-        System.out.println(String.join(" ","**       **","**       **","**        **","**        "));
-        System.out.println(String.join(" ","**       **","**       **","**       ** ","**********"));
-        System.out.println(String.join(" ","**       **","**       **","**********  ","**********"));
-        System.out.println(String.join(" ","**       **","**       **","***         ","        **"));
-        System.out.println(String.join(" "," **     ** "," **     ** ","***         ","        **"));
-        System.out.println(String.join(" ","   ****    ","   ****    ","***         ","**********"));
+        // Enhanced for-loop to iterate through the array and display the pattern
+        for (String line : bannerLines) {
+            System.out.println(line);
+        }
     }
+} {
+    
 }
