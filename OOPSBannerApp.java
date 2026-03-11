2ad249f1b3ad5d3bb230e81fcc16b38ad1642789
Thejas p <tp6959@srmist.edu.in>
2026-03-11 08:48:29 +0530

    UC7: Implemented professional OOP structure with PatternMap and Renderer

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,13 +1,6 @@
 public class OOPSBannerApp {
     public static void main(String[] args) {
         String[] banner = {
-                String.join(" ", "    ***    ", "    ***    ", " ******* ", "  ****** "),
-                String.join(" ", "  **   **  ", "  **   **  ", " **   ** ", " **    **"),
-				String.join(" ", " **     ** ", " **     ** ", " **   ** ", " **      "),
-				String.join(" ", " **     ** ", " **     ** ", " ******* ", "  ****** "),
-				String.join(" ", " **     ** ", " **     ** ", " **      ", "       **"),
-				String.join(" ", "  **   **  ", "  **   **  ", " **      ", " **    **"),
-				String.join(" ", "    ***    ", "    ***    ", " **      ", "  ****** "),
             String.join("  ", getOTop(), getOTop(), getPTop(), getSTop()),
             String.join("  ", getOMid(), getOMid(), getPMid(), getSMid1()),
             String.join("  ", getOMid(), getOMid(), getPMid(), getSTop()),
@@ -19,6 +12,16 @@ public class OOPSBannerApp {
 
         for (String line : banner) {
             System.out.println(line);
+
+    static class CharacterPatternMap {
+        private String[] patterns;
+
+        public CharacterPatternMap(String[] patterns) {
+            this.patterns = patterns;
+        }
+
+        public String getLine(int index) {
+            return patterns[index];
         }
     }
     public static String getOTop() { return " *** "; }
@@ -27,8 +30,48 @@ public class OOPSBannerApp {
     public static String getPTop() { return "**** "; }
     public static String getPMid() { return "*   *"; }
     public static String getPFoot() { return "*    "; }
+    public static void main(String[] args) {
+
+        CharacterPatternMap charO = new CharacterPatternMap(new String[]{
+            " ***** ",
+            "*     *",
+            "*     *",
+            "*     *",
+            "*     *",
+            "*     *",
+            " ***** "
+        });
+
+        CharacterPatternMap charP = new CharacterPatternMap(new String[]{
+            " ***** ",
+            "*     *",
+            "*     *",
+            " ***** ",
+            "*      ",
+            "*      ",
+            "*      "
+        });
 
     public static String getSTop() { return " ****"; }
     public static String getSMid1() { return "*    "; }
     public static String getSMid2() { return "    *"; }
+        CharacterPatternMap charS = new CharacterPatternMap(new String[]{
+            " ***** ",
+            "*      ",
+            "*      ",
+            " ***** ",
+            "      *",
+            "      *",
+            " ***** "
+        });
+
+        for (int i = 0; i < 7; i++) {
+            System.out.println(
+                charO.getLine(i) + "  " +
+                charO.getLine(i) + "  " +
+                charP.getLine(i) + "  " +
+                charS.getLine(i)
+            );
+        }
+    }
 }
