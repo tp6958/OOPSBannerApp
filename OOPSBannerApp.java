726861dd1bc9791f75f17027dfbb86111c10e3a5
Thejas p <tp6959@srmist.edu.in>
2026-03-11 08:43:18 +0530

    UC7: Implemented professional OOP structure with PatternMap and Renderer

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,16 +1,34 @@
 public class OOPSBannerApp {
-
     public static void main(String[] args) {
+        String[] banner = {
+                String.join(" ", "    ***    ", "    ***    ", " ******* ", "  ****** "),
+                String.join(" ", "  **   **  ", "  **   **  ", " **   ** ", " **    **"),
+				String.join(" ", " **     ** ", " **     ** ", " **   ** ", " **      "),
+				String.join(" ", " **     ** ", " **     ** ", " ******* ", "  ****** "),
+				String.join(" ", " **     ** ", " **     ** ", " **      ", "       **"),
+				String.join(" ", "  **   **  ", "  **   **  ", " **      ", " **    **"),
+				String.join(" ", "    ***    ", "    ***    ", " **      ", "  ****** "),
+            String.join("  ", getOTop(), getOTop(), getPTop(), getSTop()),
+            String.join("  ", getOMid(), getOMid(), getPMid(), getSMid1()),
+            String.join("  ", getOMid(), getOMid(), getPMid(), getSTop()),
+            String.join("  ", getOMid(), getOMid(), getPTop(), getSMid2()),
+            String.join("  ", getOMid(), getOMid(), getPFoot(), getSMid2()),
+            String.join("  ", getOMid(), getOMid(), getPFoot(), getSMid2()),
+            String.join("  ", getOTop(), getOTop(), getPFoot(), getSTop())
+        };
 
-        // Line 1: Top caps of O, O, P and S
-        System.out.println(String.join(" ","   ****    ","   ****    ","**********  ","**********"));
-        System.out.println(String.join(" "," **     ** "," **     ** ","*********** ","**********"));
-        System.out.println(String.join(" ","**       **","**       **","**        **","**        "));
-        System.out.println(String.join(" ","**       **","**       **","**        **","**        "));
-        System.out.println(String.join(" ","**       **","**       **","**       ** ","**********"));
-        System.out.println(String.join(" ","**       **","**       **","**********  ","**********"));
-        System.out.println(String.join(" ","**       **","**       **","***         ","        **"));
-        System.out.println(String.join(" "," **     ** "," **     ** ","***         ","        **"));
-        System.out.println(String.join(" ","   ****    ","   ****    ","***         ","**********"));
+        for (String line : banner) {
+            System.out.println(line);
+        }
     }
+    public static String getOTop() { return " *** "; }
+    public static String getOMid() { return "*   *"; }
+
+    public static String getPTop() { return "**** "; }
+    public static String getPMid() { return "*   *"; }
+    public static String getPFoot() { return "*    "; }
+
+    public static String getSTop() { return " ****"; }
+    public static String getSMid1() { return "*    "; }
+    public static String getSMid2() { return "    *"; }
 }
